9d04125df05ec4b51dc2e4cb21d7cb6cc0fd8916
Ido Kanner <idok@linesip.com>
2020-08-17 18:05:27 +0300

    working on interface

--- interface.go
+++ interface.go
@@ -1,5 +1,7 @@
 package esl
 
+import "fmt"
+
 // Current file contains the implementation of ESL.i interface.
 // the file is part of sockets.go but focuses only on the interface
 
@@ -11,4 +13,28 @@ func (s Socket) SendRecv(cmd string) (int, []byte, error) {
 	}
 
 	return s.Recv(MaxBufferSize)
+}
+
+// API sends the api commands
+func (s Socket) API(cmd string, args string) (*Message, error) {
+	_, buffer, err := s.SendRecv(fmt.Sprintf("api %s %s", cmd, args))
+
+	if err != nil {
+		return nil, err
+	}
+
+	message, err := NewMessage(buffer, true)
+	return message, err
+}
+
+// BgAPI sends the bgapi commands
+func (s Socket) BgAPI(cmd string, args string) (*Message, error) {
+	_, buffer, err := s.SendRecv(fmt.Sprintf("bgapi %s %s", cmd, args))
+
+	if err != nil {
+		return nil, err
+	}
+
+	message, err := NewMessage(buffer, true)
+	return message, err
 }
